f1dce88710ea3facb0948557a50ec69bfd6036b9
tsaleh8803 <tamersal03@gmail.com>
2023-08-28 09:56:08 +0300

    .

---
+++ FakeStoreProject/LikedProducts/Domain/LikedProduct.swift
@@ -0,0 +1,20 @@
+//
+//  LikedProduct.swift
+//  FakeStoreProject
+//
+//  Created by Mac on 25/08/2023.
+//
+
+import Foundation
+
+struct LikedProduct {
+    
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: String
+    let image: String
+    let rating: Rating?
+    let isLiked: Bool
+}
