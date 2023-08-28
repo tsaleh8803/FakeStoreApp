4578231e95b90e99bf3ab6494c824a7c1a605041
Husam Dayya <husamdayya@icloud.com>
2023-08-28 17:23:25 +0300

    Improved folder structure

--- FakeStoreProject/Controllers/TabViewController.swift
+++ FakeStoreProject/Controllers/TabViewController.swift
@@ -43,5 +43,4 @@ final class TabViewController: UITabBarController {
             rootView.popToRootViewController(animated: false)
         }
     }
-
 }
--- FakeStoreProject/Products/Domain/RemoteProduct.swift
+++ FakeStoreProject/Products/Domain/RemoteProduct.swift
@@ -9,7 +9,6 @@
 import Foundation
 
 struct RemoteProduct: Codable {
-
     let id: Int
     let title: String
     let price: Double
@@ -17,11 +16,9 @@ struct RemoteProduct: Codable {
     let category: String
     let image: String
     let rating: Rating
-
 }
 
 struct Rating: Codable {
     let rate: Double
     let count: Int
 }
-
